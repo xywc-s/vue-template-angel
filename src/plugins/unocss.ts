7f942cad642df4550349fd106a6bf0121f8b6589
易长松 <xywc_s@foxmail.com>
2022-10-19 16:53:38 +0800

    补充unocss插件注释

--- src/plugins/unocss.ts
+++ src/plugins/unocss.ts
@@ -1,5 +1,7 @@
 /**
  * @see https://github.com/unocss/unocss
+ * 默认扫描的文件: .jsx, .tsx, .vue, .md, .html, .svelte, .astro
+ * .js, .ts 默认不包含, 可在需要的文件顶部添加 @unocss-include 标识, 将文件加入扫描
  */
 import Unocss from 'unocss/vite'
 import presetUno from '@unocss/preset-uno'
