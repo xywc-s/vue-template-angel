652bfb5e6bcb53a376eba2d596fa78e1f6a87800
易长松 <xywc_s@foxmail.com>
2022-10-27 17:58:26 +0800

    user

--- src/repositories/common/user.ts
+++ src/repositories/common/user.ts
@@ -1,19 +1,23 @@
 import { ref } from 'vue'
 import { assign } from 'lodash-es'
-import { Auth, UserListData } from '@/api'
 import { useUser } from '@/stores/user'
+import { Auth, UserListData } from '@/api'
 import type { User } from '@/models/user'
 
 /**
  * 获取用户数据
  */
-export function useUserList(data: UserListData = {}) {
+export function useUserList(data: UserListData) {
   const users = ref<User[]>([])
   const userStore = useUser()
-  if (!userStore.userList.length) {
+  if (data) {
     Auth.findUserList(assign({ rows: 1000000 }, data)).then(({ rows }) => (users.value = rows))
   } else {
-    users.value = userStore.userList
+    if (!userStore.userList.length) {
+      Auth.findUserList({ rows: 1000000 }).then(({ rows }) => (users.value = rows))
+    } else {
+      users.value = userStore.userList
+    }
   }
   return users
 }
