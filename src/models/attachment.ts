c0eef75fba1eebe77bf541f16a8368dd2d8b81ad
易长松 <xywc_s@foxmail.com>
2022-11-03 15:34:27 +0800

    新增附件模型和公司模型

---
+++ src/models/attachment.ts
@@ -0,0 +1,55 @@
+import { generateEntityFromModel } from '@/models'
+
+export const attachment = {
+  createUserCode: {
+    name: '用户编号',
+    default: ''
+  },
+  createUserId: {
+    name: '用户ID',
+    default: ''
+  },
+  expireDate: {
+    name: '过期时间',
+    default: ''
+  },
+  id: {
+    name: 'ID',
+    default: ''
+  },
+  location: {
+    name: '文件URL',
+    default: ''
+  },
+  name: {
+    name: '文件名',
+    default: ''
+  },
+  remark: {
+    name: '备注',
+    default: ''
+  },
+  size: {
+    name: '文件大小',
+    default: ''
+  },
+  status: {
+    name: '文件状态',
+    default: '有效'
+  },
+  suffix: {
+    name: '扩展名',
+    default: ''
+  },
+  insertDate: {
+    name: '创建时间',
+    default: ''
+  },
+  updateDate: {
+    name: '更新时间',
+    default: ''
+  }
+}
+
+const entity = generateEntityFromModel(attachment)
+export type Attachment = typeof entity
