1737fd146549f38440b91adbdb67a8901e78521c
易长松 <xywc_s@foxmail.com>
2022-11-16 10:21:42 +0800

    fix 权限判断bug

--- src/components/ChildRoute.vue
+++ src/components/ChildRoute.vue
@@ -1,15 +1,14 @@
 <template>
   <template v-for="{ path, meta, children } in routes" :key="basePath + '/' + path">
-    <template
-      v-if="!meta?.hidden && userStore.hasPermission(meta?.permission as Permission)"
-    ></template>
-    <el-sub-menu v-if="children" :index="basePath + '/' + path">
-      <template #title>
-        <div>{{ meta?.title }}</div>
-      </template>
-      <child-route :routes="children" :base-path="basePath + '/' + path"></child-route>
-    </el-sub-menu>
-    <el-menu-item v-else :index="basePath + '/' + path">{{ meta?.title }}</el-menu-item>
+    <template v-if="!meta?.hidden && userStore.hasPermission(meta?.permission as Permission)">
+      <el-sub-menu v-if="children" :index="basePath + '/' + path">
+        <template #title>
+          <div>{{ meta?.title }}</div>
+        </template>
+        <child-route :routes="children" :base-path="basePath + '/' + path"></child-route>
+      </el-sub-menu>
+      <el-menu-item v-else :index="basePath + '/' + path">{{ meta?.title }}</el-menu-item>
+    </template>
   </template>
 </template>
 
