d0192793c480b1034644550e9c73b975ad22c720
易长松 <xywc_s@foxmail.com>
2024-01-09 11:35:01 +0800

    fix: 优化多语言设置

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,9 @@
 <script lang="ts" setup>
 import { ElConfigProvider } from 'element-plus'
 import { en, zhCn } from 'element-plus/es/locales'
-import { computed, reactive } from 'vue'
-import { useRouter } from 'vue-router/auto'
+import { computed } from 'vue'
 import { useApp } from '@/stores/app'
 
-console.log('🚀 ~ file: App.vue:15 ~ routes', useRouter().options.routes)
-
 const appStore = useApp()
-const locales: Record<string, any> = reactive({
-  en,
-  zh: zhCn
-})
-
-const locale = computed(() => {
-  const currentLocale = Object.keys(locales).find((key) => appStore.isLanguage(key))
-  return currentLocale ? locales[currentLocale] : 'zh'
-})
+const locale = computed(() => (appStore.isLanguage('en') ? en : zhCn))
 </script>
