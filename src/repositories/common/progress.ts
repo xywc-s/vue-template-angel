093e1a47bcb1f0430f502322ee0c0f568cf57b21
易长松 <xywc_s@foxmail.com>
2024-02-08 15:12:25 +0800

    清理原始通用函数

--- src/repositories/common/progress.ts
+++
@@ -1,8 +0,0 @@
-export function useProgressColors() {
-  return [
-    { color: '#f56c6c', percentage: 20 },
-    { color: '#e6a23c', percentage: 50 },
-    { color: '#1989fa', percentage: 99.99 },
-    { color: '#5cb87a', percentage: 100 }
-  ]
-}
