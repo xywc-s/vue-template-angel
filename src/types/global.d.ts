0f46a0f049c3f69e3a4924e9f4b7019626485a4c
易长松 <xywc_s@foxmail.com>
2024-02-08 15:10:16 +0800

    清理原始类型

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,34 +1,9 @@
 /* eslint no-unused-vars: 0 */
-import { PermissionCode, User } from '@angelyeast/model'
-import { VueInstance } from '@vueuse/core'
+import { MiddleApp } from '@angelyeast/model'
 import type { App } from 'vue'
 
-// TODO 放在通用文件内
 declare global {
-  // TODO 放在通用文件内
-  interface JWT extends Pick<User, ['id', 'code', 'email', 'name', 'phone', 'userName']> {
-    permissionList: PermissionCode[]
-    access_token: string
-    refresh_token: string
-    token_type: string
-    scope: string
-    expires_in: number
-    jti: string
-  }
-  interface ParentApp extends VueInstance {
-    $store: {
-      state: any
-      getters: Record<string, unknown> & {
-        language: string
-        token?: string
-        permissionList?: PermissionCode[]
-        user: JWT
-      }
-      [k: string]: any
-    }
-    $updateTagTitle: (title: string, fullPath?: string) => void
-  }
   interface Window {
-    app: App | ParentApp
+    app: App | MiddleApp
   }
 }
--- src/types/vite.d.ts
+++ src/types/vite.d.ts
@@ -1,2 +1,29 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client" />
+
+/* eslint no-unused-vars: 0 */
+interface ImportMetaEnv {
+  readonly VITE_DEV_PORT: number
+  readonly VITE_USER_TOKEN: string
+  readonly VITE_USER_CODE: string
+
+  readonly VITE_APP_NAME: string
+  readonly VITE_APP_PATH: string
+  readonly VITE_ROUTE_AUTO_PREFIX: boolean
+
+  readonly VITE_MIDDLE_LOGIN_URL: string
+  readonly VITE_WECHAT_APPID: string
+
+  readonly VITE_API_MICRO: string
+  readonly VITE_API_BFF: string
+  readonly VITE_API_MOCK: string
+
+  readonly VITE_PREVIEW_URL: string
+  readonly VITE_MINIO_PATH: string
+
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
