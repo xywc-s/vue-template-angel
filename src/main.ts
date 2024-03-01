ad3cc471995bb7a2a7c1c9263ca83c7fdc01c322
易长松 <xywc_s@foxmail.com>
2024-03-01 16:43:45 +0800

    删log

--- src/main.ts
+++ src/main.ts
@@ -11,14 +11,3 @@ const app = createApp(App).use(globalPlugins)
 app.mount('#app')
 window.app = app
 
-console.log(
-  '%c %c %c %c app has mounted! %c %c %c %o',
-  'background:#41b883 ; padding: 1px; margin: 0 1px',
-  'background:#41b883 ; padding: 1px; margin: 0 1px',
-  'background:#41b883 ; padding: 1px; margin: 0 1px',
-  'background:#41b883 ; padding: 1px; margin: 0 1px;  color: #fff',
-  'background:#41b883 ; padding: 1px; margin: 0 1px',
-  'background:#41b883 ; padding: 1px; margin: 0 1px',
-  'background:#41b883 ; padding: 1px; margin: 0 1px',
-  app
-)
