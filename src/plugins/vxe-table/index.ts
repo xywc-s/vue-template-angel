daf5eb319a3950541b974cc9033b9115d944be10
易长松 <xywc_s@foxmail.com>
2024-11-28 09:09:27 +0800

    vxe-ui配置调整

--- src/plugins/vxe-table/index.ts
+++ src/plugins/vxe-table/index.ts
@@ -1,34 +1,33 @@
-import VXETable, { VxeGlobalConfig } from 'vxe-table'
-import { VxeUI, VxePager } from 'vxe-pc-ui'
-import { isPlainObject } from 'lodash-es'
-import i18n from '../i18n'
-import 'vxe-table/lib/style.css'
-import 'vxe-pc-ui/lib/style.css'
 import VxeUIPluginRenderElement from '@vxe-ui/plugin-render-element'
 import '@vxe-ui/plugin-render-element/dist/style.css'
+import 'vxe-pc-ui/lib/style.css'
+import 'vxe-table/lib/style.css'
+import { ElInput, ElLink } from 'element-plus'
+import { isPlainObject } from 'lodash-es'
+import { VxeInput, VxePager } from 'vxe-pc-ui'
+import VXETable, { VxeUI, VxeGlobalConfig } from 'vxe-table'
+import i18n from '../i18n'
 import type { Plugin } from 'vue'
 
 const useVxeTable: Plugin<VxeGlobalConfig[]> = {
   install: (app, options) => {
-    VXETable.setConfig({
+    VxeUI.setConfig({
       /** 组件内置文本国际化 */
       i18n: (key, args) => i18n.global.t(key, args),
       /** 全局自动翻译(使用组件时自定义的文本) */
       translate: (key, args) => i18n.global.t(key, args)
     })
 
-    VxeUI.setConfig({
-      i18n: (key, args) => i18n.global.t(key, args),
-      /** 全局自动翻译(使用组件时自定义的文本) */
-      translate: (key, args) => i18n.global.t(key, args)
-    })
-
     if (isPlainObject(options)) {
-      VXETable.setConfig(options)
+      VxeUI.setConfig(options)
     }
 
+    app.component('ElLink', ElLink)
+    app.component('ElInput', ElInput)
+    app.component('VxeInput', VxeInput)
     // ElementPlus渲染器
-    VXETable.use(VxeUIPluginRenderElement)
+    VxeUI.use(VxeUIPluginRenderElement)
+
     VxeUI.component(VxePager)
     app.use(VXETable)
   }
